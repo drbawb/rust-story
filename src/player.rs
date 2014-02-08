49096566ced37632001f1590e6b0dec6b97feae4
Robert Straw <drbawb@fatalsyntax.com>
2014-02-08 14:38:01 -0600

    Impl. walking physicsA

--- src/player.rs
+++ src/player.rs
@@ -1,10 +1,21 @@
+use std::f64;
+use std::cmp;
+
 use game::graphics;
 use game::sprite;
 
+static SLOWDOWN_VELOCITY: f64 	= 0.8;
+static WALKING_ACCEL: f64 		= 0.0012;
+static MAX_VELOCITY: f64 		= 0.325;
+
 pub struct Player {
 	priv sprite: ~sprite::Sprite,
+	
 	priv x: i16,
-	priv y: i16
+	priv y: i16,
+
+	priv velocity_x: f64,
+	priv accel_x: f64
 }
 
 impl Player {
@@ -21,7 +32,25 @@ impl Player {
 			}
 		}
 
-		Player{sprite: ~quote, x: x, y: y}
+		Player{
+			sprite: ~quote, 
+			
+			x: x, 
+			y: y,
+			
+			velocity_x: 0.0,
+			accel_x: 0.0
+		}
+	}
+
+	pub fn startMovingLeft(&mut self) {
+		self.accel_x = -WALKING_ACCEL;
+	}
+	pub fn startMovingRight(&mut self) {
+		self.accel_x = WALKING_ACCEL;
+	}
+	pub fn stopMoving(&mut self) {
+		self.accel_x = 0.0;
 	}
 }
 
@@ -31,6 +60,22 @@ impl Player {
 impl sprite::Animatable for Player {
 	fn step_time(&mut self, elapsed_time: sprite::Millis) {
 		self.sprite.step_time(elapsed_time);
+		
+		let sprite::Millis(elapsed_time_ms) = elapsed_time;
+		self.x += f64::round(
+			self.velocity_x * elapsed_time_ms as f64
+		) as i16;
+
+		self.velocity_x += 
+			self.accel_x * elapsed_time_ms as f64;
+
+		if (self.accel_x < 0.0) {
+			self.velocity_x = cmp::max(self.velocity_x, -MAX_VELOCITY);
+		} else if (self.accel_x > 0.0) {
+			self.velocity_x = cmp::min(self.velocity_x, MAX_VELOCITY);
+		} else {
+			self.velocity_x *= SLOWDOWN_VELOCITY;
+		}
 	}
 }
 
