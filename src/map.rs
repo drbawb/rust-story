27326bf1a1e6a12af8c20948161e930545468c78
Robert Straw <drbawb@fatalsyntax.com>
2014-02-15 13:41:04 -0600

    Move set-position before draw so we can use a single sprite.

--- src/map.rs
+++ src/map.rs
@@ -12,9 +12,12 @@ pub struct Map {
 
 
 
-fn new_tile(graphics: &mut graphics::Graphics) -> Option<Rc<RefCell<~sprite::Updatable:>>> {
-	Some(
-		Rc::new(
+impl Map {
+	pub fn create_test_map(graphics: &mut graphics::Graphics) -> Map {
+		static num_rows: uint = 15; // 480
+		static num_cols: uint = 20; // 640
+
+		let tile = Rc::new(
 			RefCell::new(
 				~sprite::Sprite::new(
 					graphics, 
@@ -23,15 +26,8 @@ fn new_tile(graphics: &mut graphics::Graphics) -> Option<Rc<RefCell<~sprite::Upd
 					~"assets/PrtCave.bmp"
 				) as ~sprite::Updatable:
 			)
-		)
-	)
-}
-
-impl Map {
-	pub fn create_test_map(graphics: &mut graphics::Graphics) -> Map {
-		static num_rows: uint = 15; // 480
-		static num_cols: uint = 20; // 640
-
+		);
+		
 		let mut map = Map {
 			foreground_sprites: vec::from_elem(num_rows,
 				vec::from_elem(num_cols, None)
@@ -40,34 +36,28 @@ impl Map {
 	
 		// init very top row
 		for i in range(0, num_cols) {
-			map.foreground_sprites[11][i] = new_tile(graphics); // store a reference
+			map.foreground_sprites[11][i] = Some(tile.clone()); // store a reference
 		}
 
-		map.foreground_sprites[10][5] 	= new_tile(graphics);
-		map.foreground_sprites[9][4] 	= new_tile(graphics);
-		map.foreground_sprites[8][3] 	= new_tile(graphics);
-		map.foreground_sprites[7][2] 	= new_tile(graphics);
-		map.foreground_sprites[10][3] 	= new_tile(graphics);
-
-
+		map.foreground_sprites[10][3] 	= Some(tile.clone());
+		map.foreground_sprites[10][5] 	= Some(tile.clone());
+		
+		map.foreground_sprites[9][4] 	= Some(tile.clone());
+		map.foreground_sprites[8][3] 	= Some(tile.clone());
+		map.foreground_sprites[7][2] 	= Some(tile.clone());
+		
 		map
 	}
 }
 
 impl sprite::Updatable for Map {
 	fn update(&mut self, elapsed_time: sprite::Millis) {
-		for x in range(0, self.foreground_sprites.len()) {
-			for y in range(0, self.foreground_sprites[x].len()) {
-				match self.foreground_sprites[x][y] {
+		for row in self.foreground_sprites.iter() {
+			for col in row.iter() {
+				match *col {
 					Some(ref elem) => {
 						let mut sprite = elem.borrow().borrow_mut();
 						sprite.get().update(elapsed_time);
-						sprite.get().set_position(
-							(
-								(y * sprite::TILE_SIZE as uint) as i32, 
-								(x * sprite::TILE_SIZE as uint) as i32
-							)
-						);
 					}
 					_ => {}
 				};
@@ -79,14 +69,26 @@ impl sprite::Updatable for Map {
 	fn set_position(&mut self, coords: (i32,i32)) {}
 }
 
+
+
 impl sprite::Drawable for Map {
 	/// Draws current state to `display`
 	fn draw(&self, graphics: &graphics::Graphics) {
-		for row in self.foreground_sprites.iter() {
-			for col in row.iter() {
-				match *col {
+		for a in range(0, self.foreground_sprites.len()) {
+			for b in range(0, self.foreground_sprites[a].len()) {
+				match self.foreground_sprites[a][b] {
 					Some(ref elem) => {
+						// draw sprite at x,y coordinates.
+						// a => row (y-axis)
+						// b => col (x-axis)
 						let mut sprite = elem.borrow().borrow_mut();
+						sprite.get().set_position(
+							(
+								(b * sprite::TILE_SIZE as uint) as i32,
+								(a * sprite::TILE_SIZE as uint) as i32
+							)
+						);
+
 						sprite.get().draw(graphics);
 					}
 					_ => {}
