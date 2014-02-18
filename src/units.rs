4d870885d9646c27013f35730f5ee48e029f0c19
Robert Straw <drbawb@fatalsyntax.com>
2014-02-18 09:46:20 -0600

    (Doc)

--- src/units.rs
+++ src/units.rs
@@ -4,6 +4,8 @@
 pub struct Millis(uint);
 
 impl Add<Millis,Millis> for Millis {
+	/// The `uint`s inside LHS & RHS will be added together and wrapped 
+	/// inside a new `Millis()`
 	fn add(&self, rhs: &Millis) -> Millis {
 		let Millis(a) = *self;
 		let Millis(b) = *rhs;
@@ -13,6 +15,9 @@ impl Add<Millis,Millis> for Millis {
 }
 
 impl Mul<f64, f64> for Millis {
+	/// The `uint` inside LHS will be cast to f64.
+	/// Multiplication will then proceed as normal, returning
+	/// an f64 as a result.
 	fn mul(&self, rhs: &f64) -> f64 {
 		let Millis(a) = *self;
 
