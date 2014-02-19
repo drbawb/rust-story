2df7f8b1b38cc6345ac9da68b2f9567d86f8cf80
Robert Straw <drbawb@fatalsyntax.com>
2014-02-19 09:51:55 -0600

    Change units::Pixel to i32 b/c that's primarily what `rust-sdl2` uses.

--- src/units.rs
+++ src/units.rs
@@ -3,7 +3,7 @@ use std::f64;
 pub static TILE_SIZE: i32 = 32;
 
 pub type Game 	= f64;	// Resolution independent units of position.
-pub type Pixel 	= int;	// An absolute position on the screen.
+pub type Pixel 	= i32;	// An absolute position on the screen.
 pub type Tile 	= uint;	
 
 pub type Fps = uint;	// Hz or (1 / Second)
