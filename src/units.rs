51e915b8dde6a881dd5fecb9462c4752d816f3ea
Robert Straw <drbawb@fatalsyntax.com>
2014-02-19 09:03:17 -0600

    Switch visibility to public

--- src/units.rs
+++ src/units.rs
@@ -2,33 +2,33 @@ use std::f64;
 
 pub static TILE_SIZE: i32 = 32;
 
-type Game 	= f64;	// Resolution independent units of position.
-type Pixel 	= int;	// An absolute position on the screen.
-type Tile 	= uint;	
+pub type Game 	= f64;	// Resolution independent units of position.
+pub type Pixel 	= int;	// An absolute position on the screen.
+pub type Tile 	= uint;	
 
-type Fps = uint;	// Hz or (1 / Second)
+pub type Fps = uint;	// Hz or (1 / Second)
 
-type Velocity 		= f64; 	// Game / Millis()
-type Acceleration 	= f64;	// Game / Millis() / Millis()
+pub type Velocity 		= f64; 	// Game / Millis()
+pub type Acceleration 	= f64;	// Game / Millis() / Millis()
 
 #[inline]
-fn game_to_pixel(gunit: Game) -> Pixel {
+pub fn game_to_pixel(gunit: Game) -> Pixel {
 	// TODO: assuming 32px
 	return f64::round(gunit) as Pixel
 }
 
 #[inline]
-fn game_to_tile(gunit: Game) -> Tile {
+pub fn game_to_tile(gunit: Game) -> Tile {
 	(gunit / TILE_SIZE as f64) as Tile
 }
 
 #[inline]
-fn tile_to_game(tunit: Tile) -> Game {
+pub fn tile_to_game(tunit: Tile) -> Game {
 	(tunit * (TILE_SIZE as uint)) as Game
 }
 
 #[inline]
-fn tile_to_pixel(tunit: Tile) -> Pixel {
+pub fn tile_to_pixel(tunit: Tile) -> Pixel {
 	game_to_pixel(tile_to_game(tunit))
 }
 
