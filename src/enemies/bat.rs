0477b1ad331517e3effe6b029665360f81c50508
Robert Straw <drbawb@fatalsyntax.com>
2014-02-22 17:13:55 -0600

    (Use our units...)

--- src/enemies/bat.rs
+++ src/enemies/bat.rs
@@ -2,10 +2,10 @@ pub use game::sprite;
 pub use game::units;
 pub use game::graphics;
 
-static X_OFFSET: uint 	= 2;
-static Y_OFFSET: uint 	= 3;
-static SPRITE_FRAMES: uint	= 3;
-static SPRITE_FPS: uint 	= 15;
+static X_OFFSET: units::Tile 	= units::Tile(2);
+static Y_OFFSET: units::Tile 	= units::Tile(2);
+static SPRITE_FRAMES: units::Frame	= 3;
+static SPRITE_FPS: units::Fps 		= 15;
 
 pub struct CaveBat {
 	coords: (units::Game, units::Game),
@@ -20,7 +20,7 @@ impl CaveBat {
 		let asset_path = ~"assets/base/Npc/NpcCemet.bmp";
 		let asset = ~sprite::AnimatedSprite::new(
 						display, asset_path, 
-						(units::Tile(X_OFFSET), units::Tile(Y_OFFSET)), 
+						(X_OFFSET, Y_OFFSET), 
 						(units::Tile(1), units::Tile(1)),
 						SPRITE_FRAMES, SPRITE_FPS
 					).unwrap() as ~sprite::Updatable;
