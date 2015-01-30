01d9109a13e48ac1ad16399595bcc0449659b1b1
Robert Straw <drbawb@fatalsyntax.com>
2015-01-29 21:43:44 -0600

    Revamp sprite caching mechanism ...
    * Graphics now holds a reference to a renderer
    * References to textures are accessible via the sprite cache
    * The sprite cache is indexed by file-path (String)

--- src/graphics.rs
+++ src/graphics.rs
@@ -5,18 +5,31 @@ use units::{AsPixel};
 use sdl2::rect;
 use sdl2::pixels::Color;
 use sdl2::surface;
-use sdl2::render::{self, RenderDriverIndex};
+use sdl2::render::{self, Renderer, RenderDriverIndex, Texture};
 use sdl2::video::{self, WindowPos};
 use sdl2::mouse;
 
+use std::collections::hash_map::{HashMap, Entry};
+
 /// Acts as a buffer to the underlying display
-pub struct Graphics {
-	screen: render::Renderer,
+pub struct Graphics<'g> {
+	cache:  HashMap<String, Texture<'g>>,
+	screen: &'g Renderer,
 }
 
-impl Graphics {
+impl<'g> Graphics<'g> {
 	/// Prepare the display for rendering
-	pub fn new() -> Graphics {
+	pub fn new(renderer: &Renderer) -> Graphics {
+		let graphics = Graphics {
+			cache:  HashMap::new(),
+			screen: renderer,
+		};
+
+		mouse::show_cursor(true);
+		return graphics;
+	}
+
+	pub fn init_renderer() -> Renderer {
 		let (units::Pixel(w), units::Pixel(h)) = 
 			(game::SCREEN_WIDTH.to_pixel(), game::SCREEN_HEIGHT.to_pixel());
 		
@@ -32,33 +45,25 @@ impl Graphics {
 			Err(msg) => panic!(msg),
 		};
 
-		let render_context = render::Renderer::from_window(
+		 Renderer::from_window(
 			window_context,
 			RenderDriverIndex::Auto,
 			render::SOFTWARE,
-		);
-
-		let graphics: Graphics = match render_context {
-			Ok(renderer) => {
-				Graphics{ screen: renderer, }
-			},
-			Err(msg) => {panic!(msg)},
-		};
-		
-		mouse::show_cursor(false);
-		return graphics;
+		).unwrap()
 	}
 
-	/// Loads a bitmap which resides at `file_path` and returns a handle
-	/// This handle can safely be used in any of the graphics subsystem's rendering
-	/// contexts.
+	/// Caches the bitmap found at `file_path` ...
+	/// The filename can then be used to fetch a handle to the loaded
+	/// texture at a later occasion.
+	///
+	/// Panics if the resource cannot be loaded for any reason ...
 	pub fn load_image(&mut self, 
 	                  file_path: String, 
-	                  transparent_black: bool) -> render::Texture {
+	                  transparent_black: bool) {
 		
 		// Retrieve a handle or generate a new one if it exists already.
 		// Load sprite
-		let sprite_path = Path::new(file_path);
+		let sprite_path = Path::new(file_path.clone());
 		let sprite_window = surface::Surface::from_bmp(&sprite_path);
 
 		// Store sprite
@@ -75,25 +80,36 @@ impl Graphics {
 			}
 		}
 
-		match self.screen.create_texture_from_surface(&sprite_surface) {
-			Ok(texture) => texture,
-			Err(msg) => panic!("sprite could not be rendered: {}", msg)
-		}
+		match self.cache.entry(file_path) {
+			Entry::Vacant(entry) => {
+				match self.screen.create_texture_from_surface(&sprite_surface) {
+					Ok(texture) => { entry.insert(texture); },
+					Err(msg) => panic!("sprite could not be rendered: {}", msg)
+				}
+			},
+
+			_ => {},
+		};
 	}
 
-	pub fn blit_surface(&self,
-	                    src: &mut render::Texture,
+	pub fn borrow_res(&mut self, file_path: String) -> &mut Texture<'g> {
+		&mut self.cache[file_path]
+	}
+
+	pub fn blit_surface(&mut self,
+	                    src_id: &str,
 	                    src_rect:  &rect::Rect,
 	                    dest_rect: &rect::Rect) {
-		
-		let _ = self.screen.copy(src, Some(*src_rect), Some(*dest_rect));
+	
+		let src = &mut self.cache[*src_id];
+		let _ = self.screen.drawer().copy(src, Some(*src_rect), Some(*dest_rect));
 	}
 
 	pub fn switch_buffers(&self) {
-		self.screen.present();
+		self.screen.drawer().present();
 	}
 
 	pub fn clear_buffer(&self) {
-		let _ = self.screen.clear();
+		let _ = self.screen.drawer().clear();
 	}
 }
