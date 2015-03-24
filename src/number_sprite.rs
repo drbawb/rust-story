9de69a93caf426ee9834a8368a820bffe2f9d4a2
Robert Straw <drbawb@fatalsyntax.com>
2015-03-23 19:50:41 -0500

    (load and draw fixed digit 3)

--- src/number_sprite.rs
+++ src/number_sprite.rs
@@ -1,27 +1,26 @@
 use graphics::Graphics;
 use sprite::{Drawable, Sprite};
-use units::{AsGame, Game};
+use units::{self, AsGame};
 
 pub struct NumberSprite {
-	sprite: Option<Sprite>,
+	sprite: Sprite,
 }
 
 impl NumberSprite {
 	pub fn new(graphics: &mut Graphics, number: i32) -> NumberSprite {
-		// grab apropriate digit from spritesheet
-		// 	(units::HalfTile(3), units::HalfTile(7)),
-		// 	(units::HalfTile(1), units::HalfTile(1)),
-		// 	format!("assets/base/TextBox.bmp"),
-		// );
+		let digit = Sprite::new(
+			graphics,
+			(units::HalfTile(3), units::HalfTile(7)),
+			(units::HalfTile(1), units::HalfTile(1)),
+			format!("assets/base/TextBox.bmp"),
+		);
 
-		NumberSprite { sprite: None }
+		NumberSprite { sprite: digit }
 	}
 }
 
 impl<C: AsGame> Drawable<C> for NumberSprite {
 	fn draw(&mut self, display: &mut Graphics, coords: (C, C)) {
-		// normalize coordinates
-		let (x,y) = coords;
-		let (x,y) = (x.to_game(), y.to_game());
+		self.sprite.draw(display, coords);
 	}
 }
