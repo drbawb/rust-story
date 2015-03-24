0f96b3228116b6c5e4b66159f2a6a8b25844487e
Robert Straw <drbawb@fatalsyntax.com>
2015-03-23 20:09:17 -0500

    add digit iterator
    this gives us a nicely composable iterator to chop an input i32 into
    component digits.
    
    we can map over these digits to create successive sprites, and collect
    them into a vector of sprites representing our desired number.

--- src/number_sprite.rs
+++ src/number_sprite.rs
@@ -2,6 +2,33 @@ use graphics::Graphics;
 use sprite::{Drawable, Sprite};
 use units::{self, AsGame};
 
+/// Each call to `next` yields digits from successively _increasing
+/// powers of 10._ (e.g: 10, 100, 1000, etc.)
+///
+/// This can be used, for e.g, to yield the digits from a whole number.
+/// Useful for impl. counters, status bars, prompts, etc.
+///
+struct DigitIter {
+	remainder: i32,
+}
+
+impl DigitIter {
+	fn new(number: i32) -> DigitIter {
+		DigitIter { remainder: number }
+	}
+}
+
+impl Iterator for DigitIter {
+	type Item = i32;
+	fn next(&mut self) -> Option<i32> {
+		if self.remainder <= 0 { return None; }
+
+		let digit = self.remainder % 10; // grab last digit
+		self.remainder /= 10;       // truncate power of 10
+		Some(digit)                 // yield the truncated digit
+	}
+}
+
 pub struct NumberSprite {
 	sprite: Sprite,
 }
@@ -10,7 +37,7 @@ impl NumberSprite {
 	pub fn new(graphics: &mut Graphics, number: i32) -> NumberSprite {
 		let digit = Sprite::new(
 			graphics,
-			(units::HalfTile(3), units::HalfTile(7)),
+			(units::HalfTile(4), units::HalfTile(7)),
 			(units::HalfTile(1), units::HalfTile(1)),
 			format!("assets/base/TextBox.bmp"),
 		);
@@ -24,3 +51,13 @@ impl<C: AsGame> Drawable<C> for NumberSprite {
 		self.sprite.draw(display, coords);
 	}
 }
+
+#[test]
+fn test_digit_iter() {
+	let mut iter  = DigitIter::new(42);
+	let digit_10  = iter.next();
+	let digit_100 = iter.next();
+	assert_eq!(digit_10,  Some(2));
+	assert_eq!(digit_100, Some(4));
+}
+
